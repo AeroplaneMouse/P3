f19063b0dcdb5a499db0d2c631172d47169e9949
Daniel Fly <danielfly42@gmail.com>
2019-09-24 12:36:50 +0200

    Added first draft for layout

---
+++ Asset Management System/Left_navigation.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Asset_Management_System
+{
+    /// <summary>
+    /// Interaction logic for Left_navigation.xaml
+    /// </summary>
+    public partial class Left_navigation : Page
+    {
+        public Left_navigation()
+        {
+            InitializeComponent();
+        }
+    }
+}
