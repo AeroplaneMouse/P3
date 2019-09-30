b928f59db96a01a80cc2de81896ab4fb704b499c
Daniel Fly <danielfly42@gmail.com>
2019-09-30 18:24:47 +0200

    Removed unnessesary usings

--- Asset Management System/Pages/TopNavigation.xaml.cs
+++ Asset Management System/Pages/TopNavigation.xaml.cs
@@ -1,16 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Asset_Management_System;
 
 namespace Asset_Management_System
 {
@@ -25,7 +14,7 @@ namespace Asset_Management_System
         {
             InitializeComponent();
             Session session = new Session();
-            Lbl_currentUser.Content = $"{ (session.IsAdmin ? "Admin : ": "") }{ session.Username }";
+            Lbl_currentUser.Content = $"{ (session.IsAdmin ? "Admin : " : "") }{ session.Username }";
         }
     }
 }
