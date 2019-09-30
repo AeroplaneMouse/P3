0db0d1fe82afa2a1409bd443b8615520cbd4a7d9
Daniel Fly <danielfly42@gmail.com>
2019-09-30 09:08:57 +0200

    Added asset class

---
+++ Asset Management System/Asset.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asset_Management_System
+{
+    class Asset
+    {
+
+        public Asset()
+        {
+
+        }
+
+        public int ID { get; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+        public int Department_ID { get; set; }
+    }
+}
