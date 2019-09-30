6f244eb1485b5434286bb4d625df789a2f6b5c22
Daniel Fly <danielfly42@gmail.com>
2019-09-30 09:56:52 +0200

    Added comment and user classes

--- Asset Management System/Asset.cs
+++ Asset Management System/Asset.cs
@@ -7,23 +7,23 @@ namespace Asset_Management_System
     class Asset
     {
 
-        public Asset()
+        public Asset(string label, string description)
         {
-
+            Label = label;
+            Description = description;
         }
 
         public int ID { get; }
         
-        public string Name { get; set; }
+        public string Label { get; set; }
         
         public string Description { get; set; }
         
         public DateTime CreatedAt { get; set; }
         
-        public DateTime UpdatedAt { get; set; }
         
         public int DepartmentID { get; set; }
 
-        public Field[] Fields { get; set; }
+        public string Fields { get; set; }
     }
 }
---
+++ Asset Management System/Comment.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asset_Management_System
+{
+    class Comment
+    {
+        private string _comment = String.Empty;
+        public Comment(string comment)
+        {
+            _comment = comment;
+        }
+
+        public override string ToString() => _comment;
+    }
+}
