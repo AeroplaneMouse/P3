3912db5cb066d93a894287050073933b46719f94
Daniel Fly <danielfly42@gmail.com>
2019-09-30 09:14:11 +0200

    Added tag and field classes

---
+++ Asset Management System/Field.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asset_Management_System
+{
+    class Field
+    {
+        public Field()
+        {
+
+        }
+
+        public int ID { get; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public bool Required { get; set; }
+        public string Type { get; set; }
+
+    }
+}
---
+++ Asset Management System/Tag.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asset_Management_System
+{
+    class Tag
+    {
+
+        public Tag()
+        {
+
+        }
+
+        public int ID { get; }
+        public string Name { get; set; }
+        public int Department_ID { get; set; }
+        public DateTime Created_at { get; set; }
+        public DateTime Updated_at { get; set; }
+
+    }
+}
