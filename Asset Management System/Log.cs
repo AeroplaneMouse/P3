f9dd41fea626de120220b0f6c8f5b62ea8b2281a
Unknown <jakob.soenderby@hotmail.com>
2019-09-30 10:39:06 +0200

    Added Log Class
    Added Log class with properties described in class diagram

---
+++ Asset Management System/Log.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asset_Management_System
+{
+    class Log
+    {
+        public Log()
+        {
+
+        }
+
+        public int ID { get; }
+
+        public string Description { get; set; }
+
+        public DateTime RegistrationTime { get; set; }
+
+        public User DoneBy { get; set; }
+    }
+}
