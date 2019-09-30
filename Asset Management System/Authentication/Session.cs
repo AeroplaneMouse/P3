aa2f16dd64a76590d1beb65780f396d89d4b41d0
Thomas Lorentzen <thomas@tlorentzen.net>
2019-09-30 13:04:39 +0200

    WIP: Added session class

---
+++ Asset Management System/Authentication/Session.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.DirectoryServices;
+using System.Security.Principal;
+
+namespace Asset_Management_System
+{
+    class Session
+    {
+        public readonly String Username;
+        public readonly String Domain;
+        public Boolean IsAdmin = false;
+
+        public Session()
+        {
+            string[] parts = WindowsIdentity.GetCurrent().Name.Split('\\');
+            this.Domain = parts[0];
+            this.Username = parts[1];
+        }
+
+        public Boolean Validate()
+        {
+            /*
+            try
+            {
+                using (DirectoryEntry myLdapConnection = createDirectoryEntry())
+                {
+                    using (DirectorySearcher searcher = new DirectorySearcher(myLdapConnection))
+                    {
+                        searcher.Filter = String.Format("({0}={1})", "cn", "tlorentzen");
+                        var result = searcher.FindOne();
+                        if (result != null)
+                        {
+                            var displayName = result.Properties["cn"];
+                            Console.WriteLine(displayName);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                // if we get an error, it means we have a login failure.
+                // Log specific exception
+                Console.WriteLine(ex);
+            }
+            */
+            return true;
+        }
+
+        private static DirectoryEntry createDirectoryEntry()
+        {
+            DirectoryEntry ldapConnection = new DirectoryEntry();
+            ldapConnection.Path = "ldap://ldap.jumpcloud.com:389;ou=Users;o=5d8a453c62f4be26b5c4895e,dc=jumpcloud,dc=com";
+            ldapConnection.AuthenticationType = AuthenticationTypes.Secure;
+
+            return ldapConnection;
+        }
+
+        //public SqlConnection Database { get; private set; }
+    }
+}
