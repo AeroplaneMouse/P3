e1ca135ef1e63b6f864b027fb07cbcf410acd241
Thomas Lorentzen <thomas@tlorentzen.net>
2019-10-01 14:27:37 +0200

    Update DepartmentRepository.cs

--- Asset Management System/Database/Repositories/DepartmentRepository.cs
+++ Asset Management System/Database/Repositories/DepartmentRepository.cs
@@ -1,10 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Text;
-using Asset_Management_System.Models;
 using MySql.Data.MySqlClient;
+using System.Collections.Generic;
+using Asset_Management_System.Models;
 
 namespace Asset_Management_System.Database.Repositories
 {
