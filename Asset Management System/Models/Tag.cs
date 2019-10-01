fd429dc2dda1224fd37003d5cf2701b6b9a57bd9
Alexander Nykjær <alax0013@gmail.com>
2019-10-01 10:20:11 +0200

    Added updaate function to tag.

--- Asset Management System/Models/Tag.cs
+++ Asset Management System/Models/Tag.cs
@@ -5,23 +5,42 @@ namespace Asset_Management_System.Models
 {
     class Tag : DoContainFields
     {
-        public Tag(int departmentId,string name)
+        public Tag(string name,int departmentId,int parentId = 0)
         {
-            DepartmentID = departmentId;
+            DepartmentId = departmentId;
             CreatedAt = DateTime.Now;
             Name = name;
             FieldsList = new List<Field>();
+            ParentId = parentId;
+
         }
 
         public int ID { get; }
 
+        public int ParentId;
+
         public string Name { get; set; }
 
-        public int DepartmentID { get; set; }
+        public int DepartmentId { get; set; }
 
         public DateTime CreatedAt { get; set; }
 
         public DateTime UpdatedAt { get; set; }
+
+        public bool RenameTag(string newName)
+        {
+            if (newName!= null)
+            {
+                Name = newName;    
+                UpdatedAt = DateTime.Now;
+            }
+            else
+            {
+                throw new NullReferenceException();
+            }
+
+            return true;
+        }
         
     }
 }
