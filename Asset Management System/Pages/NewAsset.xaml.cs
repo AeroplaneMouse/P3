8081268549721d242679fb5dab6938e7039163bb
Daniel Fly <danielfly42@gmail.com>
2019-10-03 21:45:47 +0200

    WIP: Select new department

--- Asset Management System/Pages/NewAsset.xaml.cs
+++ Asset Management System/Pages/NewAsset.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows.Controls;
 using Asset_Management_System.Models;
 using Asset_Management_System.Database.Repositories;
 
@@ -24,7 +25,13 @@ namespace Asset_Management_System.Pages
             Asset asset = new Asset();
             asset.Label = name;
             asset.Description = description;
-
+            
+            Department department = Main.topNavigationPage.BtnShowDepartments.Content as Department;
+            if (department != null)
+                asset.DepartmentID = department.ID;
+            else
+                Console.WriteLine("ERROR! Department not found.");
+            
             AssetRepository rep = new AssetRepository();
             rep.Insert(asset);
             Main.ChangeSourceRequest(new Assets(Main));
