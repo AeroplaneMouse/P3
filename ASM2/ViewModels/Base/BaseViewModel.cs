82a126f0607ebe5634b56f52ebc3f1af5a0bfe28
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2019-10-07 15:59:23 +0200

    Early beginnings

---
+++ ASM2/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace ASM2.ViewModels.Base
+{
+    /// <summary>
+    /// Base view model that fires Property Changed events as needed
+    /// </summary>
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        // The event that is fired when any child property changes its value
+        public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
+
+        /// <summary>
+        /// Call this to fire a <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
