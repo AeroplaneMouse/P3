5f8ab331104c9e4c194ff5ce4a034661c6440244
Daniel Fly <danielfly42@gmail.com>
2019-10-12 23:24:43 +0200

    Added the base viewModel

---
+++ Asset Management System/ViewModels/Base/BaseViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Asset_Management_System.ViewModels.Base
+{
+    public class BaseViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// Call this to fire a <see cref="PropertyChanged"/> event
+        /// </summary>
+        /// <param name="name"></param>
+        public void OnPropertyChanged(string name)
+        {
+            PropertyChanged(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
