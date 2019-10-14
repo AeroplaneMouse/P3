6d1face3caecf751d800bc37328e6cb221fa3633
Michelle Terpling <michelle@terpling.dk>
2019-10-14 10:10:15 +0200

    Rettet noget

--- Asset Management System/Database/Repositories/AssetRepository.cs
+++ Asset Management System/Database/Repositories/AssetRepository.cs
@@ -260,13 +260,20 @@ namespace Asset_Management_System.Database.Repositories
             string row_options = reader.GetString("options");
             Console.WriteLine(row_options);
 
-            
-            return (Asset) Activator.CreateInstance(typeof(Asset), BindingFlags.Instance | BindingFlags.NonPublic, null,
 
+            return (Asset)Activator.CreateInstance(typeof(Asset), BindingFlags.Instance | BindingFlags.NonPublic, null,
+                new object[] { row_id, row_label, row_description, row_department_id, row_created_at, row_options }, null, null);
+        }
         public bool AttachTagsToAsset(Asset asset, List<Tag> tags)
         {
             if (dbcon.IsConnect())
             {
+                string query = "INSERT INTO asset_tags (asset_id, tag_id) VALUES ";
+
+                foreach (Tag tag in tags)
+                {
+                }
+
                 using (var cmd = new MySqlCommand(query, dbcon.Connection))
                 {
                     cmd.
@@ -282,10 +289,8 @@ namespace Asset_Management_System.Database.Repositories
                         }
                     }
                 }
-                string query = "INSERT INTO asset_tags (asset_id, tag_id) VALUES ";
-                foreach (Tag tag in tags)
-                {
-                }
+                
+                
             }
             else
                 return false;
