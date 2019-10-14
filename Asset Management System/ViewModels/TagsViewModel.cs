b46d9c0150df88c10c0d1c9dc54cf94f72ea3b5e
Alexander Nykjær <alax0013@gmail.com>
2019-10-14 13:43:05 +0200

    Can now remove tags?

--- Asset Management System/ViewModels/TagsViewModel.cs
+++ Asset Management System/ViewModels/TagsViewModel.cs
@@ -22,12 +22,14 @@ namespace Asset_Management_System.ViewModels
 
             Search();
             // Initializing commands
-            AddNewCommand = new ViewModels.Base.RelayCommand(() => _main.ChangeMainContent(new Views.TagManager(_main)));
+            AddNewCommand =
+                new ViewModels.Base.RelayCommand(() => _main.ChangeMainContent(new Views.TagManager(_main)));
             SearchCommand = new ViewModels.Base.RelayCommand(() => Search());
             EditCommand = new ViewModels.Base.RelayCommand(() => Edit());
             RemoveCommand = new ViewModels.Base.RelayCommand(() => Remove());
             PrintCommand = new Base.RelayCommand(() => Print());
         }
+
         #endregion
 
         #region Private Properties
@@ -109,7 +111,7 @@ namespace Asset_Management_System.ViewModels
             if (selectedTag != null)
             {
                 Console.WriteLine("Editing " + selectedTag.Label);
-                _main.ChangeMainContent(new TagManager(_main,selectedTag));
+                _main.ChangeMainContent(new TagManager(_main, selectedTag));
             }
             else
             {
@@ -122,36 +124,23 @@ namespace Asset_Management_System.ViewModels
         /// </summary>
         private void Remove()
         {
-            System.Collections.IList seletedAssets = null/*LvList.SelectedItems*/;
-
-            if (seletedAssets.Count == 0)
+            Tag selectedTag = GetSelectedItem();
+           
+            if (selectedTag == null)
             {
-                string message = $"You have selected { seletedAssets.Count }. This is not a valid amount!";
-                //Main.ShowNotification(null, new NotificationEventArgs(message, Brushes.Red));
+                string message = "Please select an item.";
                 Console.WriteLine(message);
-                return;
             }
             else
             {
-                foreach (Asset asset in seletedAssets)
-                {
-                    Console.WriteLine($"Removing { asset.Name }.");
-                    new AssetRepository().Delete(asset);
-                }
-
-                string message;
-                if (seletedAssets.Count > 1)
-                    message = $"Multiple assets has been removed!";
-                else
-                    message = $"{ (seletedAssets[0] as Asset).Name } has been removed!";
-
-                //Main.ShowNotification(null, new NotificationEventArgs(message, Brushes.Green));
-                Console.WriteLine(message);
-
+                Console.WriteLine($"Removing {selectedTag.Name}.");
+                new TagRepository().Delete(selectedTag);
+                
                 // Reload list
                 Search();
             }
         }
+
 
         public void Print()
         {
