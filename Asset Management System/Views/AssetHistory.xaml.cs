d484a75e50fe828e41d02eb2767a50f033cd6f91
Jakob Sønderby Kristensen <jakob.soenderby@hotmail.com>
2019-10-18 12:12:51 +0200

    Spruced up ViewHistory
    Added a viewModel and moved some of the functionality into this, also added "Escape" key command to close the window, and a select button.

--- Asset Management System/Views/AssetHistory.xaml.cs
+++ Asset Management System/Views/AssetHistory.xaml.cs
@@ -27,10 +27,10 @@ namespace Asset_Management_System.Views
         public AssetHistory(Asset asset)
         {
             InitializeComponent();
-            DataContext = this;
-            ILogRepository<Entry> rep = new LogRepository();
-            //foreach (Entry entry in rep.GetLogEntries(asset.ID, asset.GetType())) History.Add(entry);
-            History = rep.GetLogEntries(asset.ID, asset.GetType());
+            //DataContext = this;
+            DataContext = new AssetHistoryViewModel(this, asset);
+            //ILogRepository<Entry> rep = new LogRepository();
+            //History = rep.GetLogEntries(asset.ID, asset.GetType());
             this.LabelText = "History for: " + asset.Name;
         }
         
@@ -39,53 +39,22 @@ namespace Asset_Management_System.Views
             get => Label.Text;
             set => Label.Text = value;
         }
-        
-        public int SelectedItemIndex { get; set; }
-        
-        /*
-        private ObservableCollection<Entry> _list = new ObservableCollection<Entry>();
-        public ObservableCollection<Entry> History
-        {
-            get => _list;
-            set
-            {
-                _list.Clear();
-                foreach (Entry entry in value)
-                    _list.Add(entry);
-            }
-        }
-        /**/
-        public List<Entry> History { get; set; }
 
-        private void BtnOk_Click(object sender, RoutedEventArgs e)
+        private void BtnCLose_Click(object sender, RoutedEventArgs e)
         {
             DialogResult = true;
         }
 
         private void ListView_MouseDoubleClick(object sender, MouseButtonEventArgs e)
         {
-            ViewLogEntry();
+            AssetHistoryViewModel vm = this.DataContext as AssetHistoryViewModel;
+            vm.ViewCommand.Execute(null);
         }
         
-        /// <summary>
-        /// Displays the selected log entry
-        /// </summary>
-        private void ViewLogEntry()
+        private void BtnSelect_Click(object sender, RoutedEventArgs e)
         {
-            Entry selected = GetSelectedItem();
-            var dialog = new ShowEntry(selected);
-            if (dialog.ShowDialog() == true)
-            {
-                Console.WriteLine("Displaying log entry saying : " + selected.Description);
-            }
-        }
-        
-        private Entry GetSelectedItem()
-        {
-            if (History.Count == 0)
-                return null;
-            else
-                return History.ElementAt(SelectedItemIndex);
+            AssetHistoryViewModel vm = this.DataContext as AssetHistoryViewModel;
+            vm.ViewCommand.Execute(null);
         }
     }
 }
