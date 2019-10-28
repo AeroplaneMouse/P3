c84792bf87e6c4fd4a02862a8bac94f34d3f0982
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2019-10-28 09:59:59 +0100

    Implementeret CommentRepository

--- Asset Management System/Database/Repositories/CommentRepository.cs
+++ Asset Management System/Database/Repositories/CommentRepository.cs
@@ -10,25 +10,166 @@ namespace Asset_Management_System.Database.Repositories
 {
     public class CommentRepository : ICommentRepository
     {
+        private DBConnection _dbcon;
 
-        public bool Insert(Comment entity)
+        public CommentRepository()
         {
-            throw new NotImplementedException();
+            _dbcon = DBConnection.Instance();
         }
 
-        public bool Update(Comment entity)
+        public bool Insert(Comment comment)
         {
-            throw new NotImplementedException();
+            bool query_success = false;
+
+            if (_dbcon.IsConnect())
+            {
+                try
+                {
+                    const string query = "INSERT INTO comments (asset_id, username, content, updated_at)" +
+                                         "VALUES (@asset_id, @username, @content, CURRENT_TIMESTAMP())";
+
+                    using (var cmd = new MySqlCommand(query, _dbcon.Connection))
+                    {
+                        cmd.Parameters.Add("@asset_id", MySqlDbType.UInt64);
+                        cmd.Parameters["@asset_id"].Value = comment.AssetID;
+
+                        cmd.Parameters.Add("@username", MySqlDbType.String);
+                        cmd.Parameters["@username"].Value = comment.Username;
+
+                        cmd.Parameters.Add("@content", MySqlDbType.String);
+                        cmd.Parameters["@content"].Value = comment.Content;
+
+                        query_success = cmd.ExecuteNonQuery() > 0;
+                    }
+
+                }
+                catch (MySqlException e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                finally
+                {
+                    _dbcon.Close();
+                }
+            }
+
+            return query_success;
         }
 
-        public bool Delete(Comment entity)
+        public bool Update(Comment comment)
         {
-            throw new NotImplementedException();
+            bool query_success = false;
+
+            if (_dbcon.IsConnect())
+            {
+                try
+                {
+                    const string query = "UPDATE comments SET asset_id=@asset_id, username=@username, content=@content, updated_at=CURRENT_TIMESTAMP() WHERE id=@id";
+
+                    using (var cmd = new MySqlCommand(query, _dbcon.Connection))
+                    {
+                        cmd.Parameters.Add("@asset_id", MySqlDbType.UInt64);
+                        cmd.Parameters["@asset_id"].Value = comment.AssetID;
+
+                        cmd.Parameters.Add("@username", MySqlDbType.String);
+                        cmd.Parameters["@username"].Value = comment.Username;
+
+                        cmd.Parameters.Add("@content", MySqlDbType.String);
+                        cmd.Parameters["@content"].Value = comment.Content;
+
+                        cmd.Parameters.Add("@id", MySqlDbType.UInt64);
+                        cmd.Parameters["@id"].Value = comment.ID;
+
+                        query_success = cmd.ExecuteNonQuery() > 0;
+                    }
+                }
+
+                catch (MySqlException e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                finally
+                {
+                    _dbcon.Close();
+                }
+            }
+
+            return query_success;
+        }
+
+        public bool Delete(Comment comment)
+        {
+            bool query_success = false;
+
+            if (_dbcon.IsConnect())
+            {
+                try
+                {
+                    const string query = "DELETE FROM comments WHERE id=@id";
+
+                    using (var cmd = new MySqlCommand(query, _dbcon.Connection))
+                    {
+                        cmd.Parameters.Add("@id", MySqlDbType.UInt64);
+                        cmd.Parameters["@id"].Value = comment.ID;
+
+                        query_success = cmd.ExecuteNonQuery() > 0;
+                    }
+                }
+
+                catch (MySqlException e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                finally
+                {
+                    _dbcon.Close();
+                }
+            }
+
+            return query_success;
         }
 
         public Comment GetById(ulong id)
         {
-            throw new NotImplementedException();
+            Comment comment = null;
+
+            if (_dbcon.IsConnect())
+            {
+                try
+                {
+                    const string query = "SELECT id, asset_id, username, content, created_at, updated_at, deleted_at" +
+                                         "FROM comments WHERE id=@id";
+
+                    using (var cmd = new MySqlCommand(query, _dbcon.Connection))
+                    {
+                        cmd.Parameters.Add("@id", MySqlDbType.UInt64);
+                        cmd.Parameters["@id"].Value = id;
+
+                        using (var reader = cmd.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                comment = DBOToModelConvert(reader);
+                            }
+                        }
+                    }
+                }
+
+                catch (MySqlException e)
+                {
+                    Console.WriteLine(e);
+                }
+
+                finally
+                {
+                    _dbcon.Close();
+                }
+            }
+
+            return comment;
         }
 
         public Comment DBOToModelConvert(MySqlDataReader reader)
