915805d854514e76da48a7a353997917aa43d8a4
Niels Vistisen <42961494+Nielswps@users.noreply.github.com>
2019-10-29 11:25:31 +0100

    Delete query builder added and working

--- Asset Management System/Database/QueryGenerator.cs
+++ Asset Management System/Database/QueryGenerator.cs
@@ -25,6 +25,10 @@ namespace Asset_Management_System.Database
             _query = new StringBuilder();
         }
 
+        /// <summary>
+        /// Adds a table to the query
+        /// </summary>
+        /// <param name="name"></param>
         public void AddTable(string name)
         {
             Tables.Add(new Table(name));
@@ -35,16 +39,30 @@ namespace Asset_Management_System.Database
             
         }
 
+        /// <summary>
+        /// Adds a 'where' condition to the query, based on column, value, and operator
+        /// </summary>
+        /// <param name="column"></param>
+        /// <param name="value"></param>
+        /// <param name="operators"></param>
         public void Where(string column, string value, string operators="=")
         {
             WhereStatements.Add(new Statement(column, value, operators));
         }
-        
+
+        /// <summary>
+        /// Adds a 'where' condition to the query, based on a statement
+        /// </summary>
+        /// <param name="statement"></param>
         public void Where(Statement statement)
         {
             WhereStatements.Add(statement);
         }
 
+        /// <summary>
+        /// Returns the columns contained in the query
+        /// </summary>
+        /// <returns>Comma separated columns of the query</returns>
         private string GetColumns()
         {
             if (Columns != null && Columns.Count > 0)
@@ -55,6 +73,10 @@ namespace Asset_Management_System.Database
             return "*";
         }
 
+        /// <summary>
+        /// Creates a query to select the columns of the query from the tables of the query
+        /// </summary>
+        /// <returns>Select query</returns>
         public string PrepareSelect()
         {
             _query.Append("SELECT " + GetColumns());
@@ -68,6 +90,10 @@ namespace Asset_Management_System.Database
             return _query.ToString();
         }
 
+        /// <summary>
+        /// Creates a query to insert a new row into the first table of the query, with the values of the query
+        /// </summary>
+        /// <returns>Insert query</returns>
         public string PrepareInsert()
         {
             if (Tables.Count > 0 && Columns.Count == Values.Count)
@@ -91,6 +117,10 @@ namespace Asset_Management_System.Database
             return "";
         }
 
+        /// <summary>
+        /// Creates a query to update the columns of the query for an element in the first table of the query, with the values of the query
+        /// </summary>
+        /// <returns>Update query</returns>
         public string PrepareUpdate()
         {
             //Checks if there is added any tables and if the number of columns and values are the same, to ensure success
@@ -122,11 +152,29 @@ namespace Asset_Management_System.Database
             return "";
         }
 
+        /// <summary>
+        /// Creates a query to delete an element in the table of the query
+        /// </summary>
+        /// <returns>Delete query</returns>
         public string PrepareDelete()
         {
+            //Checks if there is added any tables and if the number of columns and values are the same, to ensure success
+            if (Tables.Count > 0 && Columns.Count == Values.Count)
+            {
+                //Create the query string
+                _query.Append("DELETE FROM " + Tables[0].Name);
+                if (WhereStatements.Count > 0)
+                {
+                    _query.Append(" WHERE " + string.Join(" AND ", from item in WhereStatements select item.Render()));
+                }
+                return _query.ToString();
+            }
             return "";
         }
         
+        /// <summary>
+        /// 
+        /// </summary>
         public void Dispose()
         {
         }
---
+++ UnitTests/QueryGeneratorTests.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Asset_Management_System.Database.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Asset_Management_System.Models;
+using Asset_Management_System.Database;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class QueryGeneratorTests
+    {
+        [TestMethod]
+        public void PrepareUpdate_SetNameOfAsset3ToNewName_BuildsQueryString()
+        {
+            //Arrange
+            QueryGenerator query = new QueryGenerator();
+            query.AddTable("assets");
+            query.Columns.AddRange(new[] { "name" });
+            query.Values.AddRange(new[] { "NewName" });
+            query.Where("name", "Asset #3");
+            string expected = "UPDATE assets SET name = 'NewName' WHERE name = 'Asset #3'";
+
+            //Act
+            string result = query.PrepareUpdate();
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void PrepareUpdate_SetLabelAndColorOfTag_BuildsQueryString()
+        {
+            //Arrange
+            QueryGenerator query = new QueryGenerator();
+            query.AddTable("tags");
+            query.Columns.AddRange(new[] { "label", "color" });
+            query.Values.AddRange(new[] { "NewLabel", "#666666" });
+            query.Where("label", "QueryGeneratorTestsTag");
+            string expected = "UPDATE tags SET label = 'NewLabel', color = '#666666' WHERE label = 'QueryGeneratorTestsTag'";
+
+            //Act
+            string result = query.PrepareUpdate();
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void PrepareInsert_InsertNewTagIntoDatabase_BuildsQueryString()
+        {
+            //Arrange
+            QueryGenerator query = new QueryGenerator();
+            query.AddTable("tags");
+            query.Columns.AddRange(new[] { "label", "color", "parent_id", "department_id", "options" });
+            query.Values.AddRange(new[] { "QueryGeneratorTestsTag", "#666666", "1", "20", "[]" });
+            string expected = "INSERT INTO tags ( label, color, parent_id, department_id, options ) VALUES ( 'QueryGeneratorTestsTag', '#666666', 1, 20, '[]' )";
+
+            //Act
+            string result = query.PrepareInsert();
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void PrepareSelect_SelectLabelAndColorFromTagTable_BuildsQueryString()
+        {
+            //Arrange
+            QueryGenerator query = new QueryGenerator();
+            query.AddTable("tags");
+            query.Columns.AddRange(new[] { "label", "color" });
+            query.Where("id", "1");
+            string expected = "SELECT label, color FROM tags WHERE id = 1";
+
+            //Act
+            string result = query.PrepareSelect();
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+
+        [TestMethod]
+        public void PrepareDelete_DeleteTagFromDatabase_BuildsQueryString()
+        {
+            //Arrange
+            QueryGenerator query = new QueryGenerator();
+            query.AddTable("tags");
+            query.Where("id", "1");
+            string expected = "DELETE FROM tags WHERE id = 1";
+
+            //Act
+            string result = query.PrepareDelete();
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
