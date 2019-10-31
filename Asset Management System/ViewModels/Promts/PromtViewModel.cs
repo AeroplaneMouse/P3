5f5b5c43995fbb4a27e703b2a4477418a72ae601
Daniel Fly <danielfly42@gmail.com>
2019-10-31 10:42:04 +0100

    WIP: Added promt

---
+++ Asset Management System/ViewModels/Promts/PromtViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows.Input;
+using Asset_Management_System.Events;
+
+namespace Asset_Management_System.ViewModels.Promts
+{
+    public abstract class PromtViewModel : Base.BaseViewModel, IPromt
+    {
+        public abstract event PromtEventHandler PromtElapsed;
+        public ICommand AcceptCommand { get; set; }
+        public ICommand CancelCommand { get; set; }
+
+        public string MessageText { get; set; }
+
+
+        public PromtViewModel(string message)
+        {
+            MessageText = message;
+
+            // Initializing commands
+            AcceptCommand = new Base.RelayCommand(Accept);
+            CancelCommand = new Base.RelayCommand(Cancel);
+        }
+
+        protected abstract void Accept();
+
+        protected abstract void Cancel();
+
+    }
+}
