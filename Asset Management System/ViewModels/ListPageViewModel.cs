f9cb1014f355cf79d9474d4f96222ebb43df88d2
Daniel Fly <danielfly42@gmail.com>
2019-11-04 14:52:19 +0100

    Cleanup

--- Asset Management System/ViewModels/ListPageViewModel.cs
+++ Asset Management System/ViewModels/ListPageViewModel.cs
@@ -148,24 +148,13 @@ namespace Asset_Management_System.ViewModels
             if (selected != null)
             {
                 if (selected is Tag)
-                {
                     Main.ChangeMainContent(new ObjectViewer(Main, selected as Tag));
-                }
-
                 else if (selected is Asset)
-                {
                     Main.ChangeMainContent(new ObjectViewer(Main, selected as Asset));
-                }
-
                 else if (selected is Entry)
-                {
                     new ShowEntry(selected as Entry).ShowDialog();
-                }
-
                 else
-                {
-                    Console.WriteLine("Error when viewing");
-                }
+                    _main.AddNotification(new Notification("ERROR! Unknown error occured when trying to view.", Notification.ERROR), 3000);
             }
         }
 
