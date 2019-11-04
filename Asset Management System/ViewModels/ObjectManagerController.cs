8d3762e3727aeb219f6f28755ae04fb4a649a8e2
Alexander Nykjær <alax0013@gmail.com>
2019-11-04 10:52:54 +0100

    Added small tweak to ObjectManagerController, to ensure hiddenfields, as well as normal fields gets populated.

--- Asset Management System/ViewModels/ObjectManagerController.cs
+++ Asset Management System/ViewModels/ObjectManagerController.cs
@@ -21,14 +21,21 @@ namespace Asset_Management_System.ViewModels
             }
         }
 
-        
+
         private void ShowIfNewField(Tag tag)
+        {
+            FieldTagsPopulator(tag, FieldsList);
+            FieldTagsPopulator(tag, HiddenFields);
+        }
+
+        private void FieldTagsPopulator(Tag tag,
+            ObservableCollection<ShownField> listOffFields)
         {
             foreach (var currentTagField in tag.FieldsList)
             {
                 bool alreadyExists = false;
 
-                foreach (var shownField in this.FieldsList)
+                foreach (var shownField in listOffFields)
                 {
                     if (shownField.ShownFieldToFieldComparator(currentTagField))
                     {
@@ -53,16 +60,22 @@ namespace Asset_Management_System.ViewModels
                     }
                 }
 
-                if (alreadyExists) continue;
-                if (currentTagField.IsHidden)
+                if (alreadyExists == false)
                 {
-                    HiddenFields.Add(new ShownField(currentTagField));
-                }
-                else
-                {
-                    if (HiddenFields.FirstOrDefault(p => Equals(p.Field, currentTagField)) == null)
+                    if (currentTagField.IsHidden)
                     {
-                        FieldsList.Add(new ShownField(currentTagField));
+                        HiddenFields.Add(new ShownField(currentTagField));
+                    }
+                    else
+                    {
+                        if (HiddenFields.FirstOrDefault(p => Equals(p.Field, currentTagField)) == null)
+                        {
+                            if (FieldsList.SingleOrDefault(field => field.Field.HashId == currentTagField.HashId) ==
+                                null)
+                            {
+                                FieldsList.Add(new ShownField(currentTagField));
+                            }
+                        }
                     }
                 }
             }
