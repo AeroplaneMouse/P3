cdc3e18ba4e4ba3035212e5190bb249b1437cc4d
Thomas Lorentzen <thomas@tlorentzen.net>
2019-11-04 15:36:02 +0100

    Create Tagging.cs

---
+++ Asset Management System/Functions/Tagging.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Asset_Management_System.Database.Repositories;
+using Asset_Management_System.Views;
+using Asset_Management_System.Models;
+
+namespace Asset_Management_System.Functions
+{
+    public class Tagging
+    {
+        private List<Tag> _tags;
+        private List<Tag> _tagged;
+        
+        public Tagging()
+        {
+            Reload();
+        }
+
+        public void Reload()
+        {
+            TagRepository rep = new TagRepository();
+            _tags = (List<Tag>) rep.GetAll();
+        }
+
+        public List<Tag> Suggest(string input)
+        {
+            return null;
+        }
+
+        public void Attach(int id)
+        {
+            
+        }
+
+        public List<Tag> Tags()
+        {
+            return _tagged;
+        }
+    }
+}
