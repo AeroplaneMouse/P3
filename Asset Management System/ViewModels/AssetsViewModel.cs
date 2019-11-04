e42f52f9f22743502c9f5f0d1d66c9e391457fa2
Daniel Fly <danielfly42@gmail.com>
2019-11-04 14:48:09 +0100

    WIP: Added currentGroup

--- Asset Management System/ViewModels/AssetsViewModel.cs
+++ Asset Management System/ViewModels/AssetsViewModel.cs
@@ -11,18 +11,67 @@ using System.Windows.Media;
 using Asset_Management_System.Helpers;
 using Asset_Management_System.Logging;
 using Asset_Management_System.Resources.DataModels;
+using System.Windows;
 
 namespace Asset_Management_System.ViewModels
 {
     public class AssetsViewModel : ChangeableListPageViewModel<AssetRepository, Asset>
     {
-        private MainViewModel _main;
+        private string _currentGroup = String.Empty;
+        private string _searchQueryText = String.Empty;
+        private bool IsTagMode = false;
+
         public int ViewType => 1;
+        public Visibility IsCurrentGroupVisible { get; set; } = Visibility.Hidden;
+        public ICommand DeleteCommand { get; set; }
+
+        public string CurrentGroup
+        {
+            get => _currentGroup;
+            set
+            {
+                _currentGroup = value;
+                if (value == String.Empty)
+                    IsCurrentGroupVisible = Visibility.Hidden;
+                else
+                    IsCurrentGroupVisible = Visibility.Visible;
+            }
+        }
+
+        public new string SearchQueryText
+        {
+            get => _searchQueryText;
+            set
+            {
+                _searchQueryText = value;
+                if (value == "#" && !IsTagMode)
+                {
+                    _searchQueryText = "";
+                    CurrentGroup = "#";
+                    IsTagMode = true;
+                }
+            }
+        }
+
+
 
         public AssetsViewModel(MainViewModel main, ListPageType pageType) : base(main, pageType)
         {
-            _main = main;
-            Title = "Assets";
+            DeleteCommand = new Base.RelayCommand(Delete);
+        }
+
+        private void Delete()
+        {
+            if (CurrentGroup != String.Empty)
+            {
+                if (CurrentGroup == "#")
+                {
+                    CurrentGroup = String.Empty;
+                    IsTagMode = false;
+                }
+                else
+                    CurrentGroup = "#";
+            }
         }
 
         protected override void Search()
