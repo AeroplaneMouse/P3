4e8fc498728b02273c739f8f4d490d483b35f600
Thomas Lorentzen <thomas@tlorentzen.net>
2019-11-04 09:14:13 +0100

    Cleanup

--- Asset Management System/ViewModels/TagsViewModel.cs
+++ Asset Management System/ViewModels/TagsViewModel.cs
@@ -1,14 +1,5 @@
 ﻿using Asset_Management_System.Database.Repositories;
 using Asset_Management_System.Models;
-using System;
-using System.Collections.ObjectModel;
-using System.Linq;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Input;
-using Asset_Management_System.Helpers;
-using Asset_Management_System.Resources;
-using Asset_Management_System.Views;
 using Asset_Management_System.Resources.DataModels;
 
 namespace Asset_Management_System.ViewModels
@@ -21,7 +12,5 @@ namespace Asset_Management_System.ViewModels
         {
             Title = "Tags";
         }
-
-
     }
 }
