3233fbfaf0d22ee3a3d29b80f6851669e02bd157
Daniel Fly <danielfly42@gmail.com>
2019-11-05 23:30:31 +0100

    Cleanup

--- Asset Management System/ViewModels/Commands/AddFieldCommand.cs
+++ Asset Management System/ViewModels/Commands/AddFieldCommand.cs
@@ -12,12 +12,11 @@ namespace Asset_Management_System.ViewModels.Commands
 {
     class AddFieldCommand : ICommand
     {
-        private FieldsController _viewModel;
         private MainViewModel _main;
-        public event EventHandler CanExecuteChanged;
-
+        private FieldsController _viewModel;
         private readonly bool _isCustom;
 
+        public event EventHandler CanExecuteChanged;
 
         public AddFieldCommand(MainViewModel main, FieldsController viewModel, bool isCustom = false)
         {
@@ -34,62 +33,6 @@ namespace Asset_Management_System.ViewModels.Commands
         public void Execute(object parameter)
         {
             _main.DisplayPrompt(new Views.Prompts.CustomField(null, AddNewFieldConfirmed));
-
-
-
-            //string fieldToAdd = parameter.ToString();
-
-
-            //// Getting label, default value and is required
-            //List<string> promptResults = _viewModel.PromptManager("Text box", out var required);
-            //int fieldType = 0;
-            //bool correctPrompt = promptResults.Count > 0;
-
-            //switch (fieldToAdd)
-            //{
-            //    case "Text Field":
-            //        Console.WriteLine("Textfield added");
-            //        fieldType = 1;
-            //        break;
-            //    case "String Field":
-            //        Console.WriteLine("StringField added");
-            //        fieldType = 2;
-            //        break;
-            //    case "Integer Field":
-            //        Console.WriteLine("IntegerField added");
-            //        fieldType = 3;
-            //        break;
-            //    case "Date Field":
-            //        Console.WriteLine("Date Field added");
-            //        fieldType = 4;
-            //        break;
-            //    case "Boolean Field":
-            //        Console.WriteLine("BooleanField added");
-            //        fieldType = 5;
-            //        break;
-            //    default:
-            //        throw new NotSupportedException();
-            //}
-
-            //if (fieldType != 0 && correctPrompt)
-            //{
-            //    string defaultValue = "";
-            //    string content = "";
-            //    if (_isCustom)
-            //    {
-            //        defaultValue = promptResults[1];
-            //        content = promptResults[1];
-            //    }
-            //    else
-            //    {
-            //        defaultValue = promptResults[1];
-            //    }
-
-            //    ShownField shownField = new ShownField(new Field(promptResults[0], content, fieldType,
-            //        defaultValue,
-            //        required, _isCustom));
-            //    _viewModel.FieldsList.Add(shownField);
-            //}
         }
 
         private void AddNewFieldConfirmed(object sender, PromptEventArgs e)
