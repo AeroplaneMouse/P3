7e5c52528851a5522fc8a87f6341db1cb8ec067a
Thomas Lorentzen <thomas@tlorentzen.net>
2019-11-08 13:24:10 +0100

    Delete Tagged.cs

--- Asset Management System/Models/Tagged.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Asset_Management_System.Models
-{
-    public class Tagged
-    {
-        public ulong AssetId { get; set; }
-        public ulong TagableId { get; set; }
-        public string TagableType { get; set; }
-    }
-}
