faa24993769253f309091f9208aae049803affe3
Niels Vistisen <42961494+Nielswps@users.noreply.github.com>
2019-11-11 17:04:05 +0100

    Making parts of the code less horizontal and more vertical

--- Asset Management System/ViewModels/AssetManagerViewModel.cs
+++ Asset Management System/ViewModels/AssetManagerViewModel.cs
@@ -185,12 +185,12 @@ namespace Asset_Management_System.ViewModels
 
             #endregion
 
-            UnTagTagCommand = new UntagTagCommand(this);
+            UntagTagCommand = new UntagTagCommand(this);
         }
 
         public ICommand SaveAssetCommand { get; set; }
         public ICommand SaveMultipleAssetsCommand { get; set; }
-        public static ICommand UnTagTagCommand { get; set; }
+        public static ICommand UntagTagCommand { get; set; }
         public ICommand CancelCommand { get; set; }
 
         /// <summary>
