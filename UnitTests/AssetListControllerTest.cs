99541fe8d756566a77a26986e4e0c65dabe26e90
Jakob Sønderby Kristensen <jakob.soenderby@hotmail.com>
2019-11-18 13:10:00 +0100

    Added more test to AssetControllerTests
    Created and used mocks to test the use of dependencies

--- UnitTests/AssetListControllerTest.cs
+++ UnitTests/AssetListControllerTest.cs
@@ -1,4 +1,6 @@
-﻿using AMS.Controllers;
+﻿using System.Collections.Generic;
+using System.Linq;
+using AMS.Controllers;
 using AMS.Controllers.Interfaces;
 using AMS.Database.Repositories.Interfaces;
 using AMS.Interfaces;
@@ -7,6 +9,7 @@ using AMS.Services.Interfaces;
 using Castle.Core.Internal;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using ITagController = Asset_Management_System.ViewModels.Controllers.Interfaces.ITagController;
 
 namespace UnitTests
 {
@@ -16,17 +19,16 @@ namespace UnitTests
         private IAssetListController _assetListController;
         private IExporter _exporter;
         private IAssetRepository _assetRepository;
+        private Mock<IAssetRepository> assetRepMock;
 
         [TestInitialize]
         public void InitiateAssets()
         {
-            //TODO: Create mocks for dependencies
             // Change this line to change the class that is tested
-            //_assetListController = new AssetListController(_assetService, _exporter);
-            //Asset asset1 = new Asset {Name = "asset1"};
-            //Asset asset2 = new Asset {Name = "asset2"};
-            //_assetListController.AssetList.Add(asset1);
-            //_assetListController.AssetList.Add(asset2);
+            assetRepMock = new Mock<IAssetRepository>();
+            assetRepMock.Setup(p => p.Delete(It.IsAny<Asset>())).Returns(true);
+            // This creates a new instance of the class for each test
+            _assetListController = new AssetListController(assetRepMock.Object, _exporter);
         }
 
         [TestMethod]
@@ -34,9 +36,8 @@ namespace UnitTests
         {
             //Arrange
             int expected = 1;
-
+            
             //Act
-            _assetListController = new AssetListController(_assetRepository, _exporter);
             _assetListController.AddNew();
             int result = _assetListController.AssetList.Count;
 
@@ -51,7 +52,6 @@ namespace UnitTests
             int expected = 2;
 
             //Act
-            _assetListController = new AssetListController(_assetRepository, _exporter);
             _assetListController.AddNew();
             _assetListController.AddNew();
             int result = _assetListController.AssetList.Count;
@@ -65,13 +65,12 @@ namespace UnitTests
         {
             //Arrange
             int expected = 1;
-
-            //Act
-            _assetListController = new AssetListController(_assetRepository, _exporter);
             Asset asset1 = new Asset {Name = "asset1"};
             Asset asset2 = new Asset {Name = "asset2"};
             _assetListController.AssetList.Add(asset1);
             _assetListController.AssetList.Add(asset2);
+
+            //Act
             _assetListController.Remove(asset1);
 
             int result = _assetListController.AssetList.Count;
@@ -85,11 +84,10 @@ namespace UnitTests
         {
             //Arrange
             bool expected = true;
-
-            //Act
-            _assetListController = new AssetListController(_assetRepository, _exporter);
             Asset asset1 = new Asset {Name = "asset1"};
             _assetListController.AssetList.Add(asset1);
+
+            //Act
             _assetListController.Remove(asset1);
 
             bool result = _assetListController.AssetList.IsNullOrEmpty();
@@ -97,7 +95,59 @@ namespace UnitTests
             //Assert
             Assert.AreEqual(expected, result);
         }
+        
+        [TestMethod]
+        public void Remove_RemoveCallsRepository_ReturnsTrue()
+        {
+            //Arrange
+            Asset asset1 = new Asset {Name = "asset1"};
+            _assetListController.AssetList.Add(asset1);
 
+            //Act
+            _assetListController.Remove(asset1);
 
+            //Assert
+            // Verify that the method IAssetRepository.Delete(Asset) is called once
+            assetRepMock.Verify((p => p.Delete(It.IsAny<Asset>())), Times.Once());
+        }
+        
+        [TestMethod]
+        public void Remove_RemoveAssetNotInListDoesNotCallRepository_ReturnsTrue()
+        {
+            //Arrange
+            Asset asset1 = new Asset {Name = "asset1"};
+            Asset asset2 = new Asset {Name = "asset2"};
+            _assetListController.AssetList.Add(asset1);
+            _assetListController.AssetList.Add(asset2);
+            Asset asset3 = new Asset {Name = "asset3"};
+
+            //Act
+            _assetListController.Remove(asset3);
+
+            //Assert
+            // Verify that the method IAssetRepository.Delete(Asset) is never called
+            assetRepMock.Verify((p => p.Delete(It.IsAny<Asset>())), Times.Never);
+        }
+        
+        [TestMethod]
+        public void Remove_RemoveAssetNotInListDoesNotChangeList_ReturnsTrue()
+        {
+            //Arrange
+            bool expected = true;
+            Asset asset1 = new Asset {Name = "asset1"};
+            Asset asset2 = new Asset {Name = "asset2"};
+            _assetListController.AssetList.Add(asset1);
+            _assetListController.AssetList.Add(asset2);
+            List<Asset> list = _assetListController.AssetList;
+            Asset asset3 = new Asset {Name = "asset3"};
+
+            //Act
+            _assetListController.Remove(asset3);
+
+            bool result = _assetListController.AssetList.SequenceEqual(list);
+
+            //Assert
+            Assert.AreEqual(expected, result);
+        }
     }
 }
