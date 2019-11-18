5d4d464a043da52c323e1c07ea93e3355b14150d
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2019-11-18 09:22:33 +0100

    Unit unit unit test!

--- UnitTests/UserTests.cs
+++ UnitTests/UserTests.cs
@@ -1,4 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using AMS.Controllers;
+using AMS.Controllers.Interfaces;
+using AMS.Interfaces;
+using AMS.IO;
+using AMS.Services;
+using AMS.Services.Interfaces;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,10 +14,23 @@ namespace UnitTests
     [TestClass]
     class UserTests
     {
+        private IUserService _userService { get; set; }
+
+        private IUserImporter _userImporter { get; set; }
+
+        private IUserListController _userListController { get; set; }
+
         [TestInitialize]
         void InitializeUserTest()
         {
-            
+            _userService = new UserService();
+            _userImporter = new UserImporter(_userService);
+
+            _userListController = new UserListController(_userImporter, _userService);
+
+
+            // Make test file
+
         }
 
         [TestMethod]
@@ -23,9 +42,10 @@ namespace UnitTests
         #region UserImporter
         
 
+
         #endregion
 
-        #region UserList
+        #region UserListController
 
 
 
