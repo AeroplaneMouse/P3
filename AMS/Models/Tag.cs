62e0c978f4e9def750a57a80e8daa13afaafea20
Alexander Nykjær <alax0013@gmail.com>
2019-11-18 11:16:27 +0100

    Fixed tags?

--- AMS/Models/Tag.cs
+++ AMS/Models/Tag.cs
@@ -4,11 +4,37 @@ using System.Text;
 
 namespace AMS.Models
 {
-    public class Tag : Model
+    public class Tag :ContainsFields
     {
-        public void SerializeFields()
+        public Tag()
         {
-            throw new NotImplementedException();
+            
         }
+
+        /*Constructor used by DB*/
+        private Tag(ulong id, string name, ulong department_id, ulong parent_id, string color, int numOfChildren, DateTime created_at, DateTime updated_at, string SerializedField)
+        {
+            ID = id;
+            Name = name;
+            DepartmentID = department_id;
+            ParentID = parent_id;
+            Color = color;
+            NumOfChildren = numOfChildren;
+            this.SerializedFields = SerializedField;
+            CreatedAt = created_at;
+            UpdatedAt = updated_at;
+        }
+        
+        public string Name { get; set; }
+
+        public string Color { get; set; }
+
+        public ulong ParentID { get; set; }
+
+        public ulong DepartmentID { get; set; }
+
+        public int NumOfChildren { get; set; }
+
+        public override string ToString() => Name;
     }
 }
