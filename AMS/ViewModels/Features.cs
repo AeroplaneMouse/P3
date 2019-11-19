460b569489b0e1ab6f193cdd01ab8892c753644f
Daniel Fly <danielfly42@gmail.com>
2019-11-19 13:07:53 +0100

    Added features

---
+++ AMS/ViewModels/Features.cs
@@ -0,0 +1,50 @@
+﻿using AMS.Authentication;
+using AMS.Models;
+using AMS.ViewModels.Prompts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Controls;
+
+namespace AMS.ViewModels
+{
+    static class Features
+    {
+        public static MainViewModel Main;
+
+        // Notifications
+        public static void AddNotification(Notification n, int displayTime = 2500)
+        {
+            Main.AddNotification(n, displayTime);
+        }
+
+        // Navigation
+        public static bool NavigatePage(Page page)
+        {
+            return Main.ContentFrame.Navigate(page);
+        }
+
+        public static bool NavigateBack()
+        {
+            if (Main.ContentFrame.CanGoBack)
+            {
+                Main.ContentFrame.GoBack();
+                return true;
+            }
+            else
+                return false;
+        }
+
+        // Prompts
+        public static void DisplayPrompt(Page prompt)
+        {
+            Main.DisplayPrompt(prompt);
+        }
+
+        // Session
+        public static Session GetCurrentSession()
+        {
+            return Main.CurrentSession;
+        }
+    }
+}
