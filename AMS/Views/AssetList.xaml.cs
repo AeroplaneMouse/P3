3822ccfd832b76eb2e72af8fd30a2cac2853082f
Daniel Fly <danielfly42@gmail.com>
2019-11-21 11:29:28 +0100

    Added multiselection with remove

--- AMS/Views/AssetList.xaml.cs
+++ AMS/Views/AssetList.xaml.cs
@@ -1,12 +1,11 @@
-﻿using System.Windows.Controls;
-using AMS.Controllers;
-using AMS.Controllers.Interfaces;
-using AMS.Database.Repositories;
-using AMS.Database.Repositories.Interfaces;
-using AMS.Helpers;
+﻿using AMS.ViewModels;
 using AMS.Interfaces;
-using AMS.IO;
-using AMS.ViewModels;
+using AMS.Controllers;
+using System.Windows.Controls;
+using AMS.Database.Repositories.Interfaces;
+using System.Collections;
+using AMS.Models;
+using System.Windows;
 
 namespace AMS.Views
 {
@@ -19,6 +18,25 @@ namespace AMS.Views
         {
             InitializeComponent();
             DataContext = new AssetListViewModel(main, new AssetListController(assetRepository, exporter));
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            AssetListViewModel viewModel = (DataContext as AssetListViewModel);
+            viewModel.SelectedItems.Clear();
+
+            foreach (Asset asset in (sender as ListView).SelectedItems)
+                viewModel.SelectedItems.Add(asset);
+
+            // Setting single item selected visibility
+            viewModel.SingleSelected = viewModel.SelectedItems.Count == 1 ?
+                Visibility.Visible :
+                Visibility.Collapsed;
+
+            // Setting multiple item selected visibility
+            viewModel.MultipleSelected = viewModel.SelectedItems.Count > 0 ?
+                    Visibility.Visible :
+                    Visibility.Collapsed;
         }
     }
 }
