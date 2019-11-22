582d26ca3205525748da4606b1c8e02e050f544f
Michelle Terpling <michelle@terpling.dk>
2019-11-22 09:43:54 +0100

    !: Auto stash before merge of "The-Grand-Restructure!" and "origin/The-Grand-Restructure!"

--- AMS/ViewModels/AssetPresenterViewModel.cs
+++ AMS/ViewModels/AssetPresenterViewModel.cs
@@ -3,16 +3,32 @@ using AMS.Interfaces;
 using AMS.Models;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
 
 namespace AMS.ViewModels
 {
     class AssetPresenterViewModel : Base.BaseViewModel
     {
-        public string PageTitle { get; set; }
+        public string Name { get; set; }
+        public string Identifier { get; set; }
+        public string Description { get; set; }
+        public List<ITagable> TagList { get; set; }
+        public ObservableCollection<Field> FieldList { get; set; }
+
+        public ICommentListController CommentListController { get; set; }
+
         public AssetPresenterViewModel(Asset asset, List<ITagable> tagList, ICommentListController commentListController)
         {
-            PageTitle = asset.Name;
+            Name = asset.Name;
+            Identifier = asset.Identifier;
+            Description = asset.Description;
+            FieldList = asset.FieldList;
+
+            TagList = tagList;
+
+            CommentListController = commenListtController;
+
         }
     }
 }
