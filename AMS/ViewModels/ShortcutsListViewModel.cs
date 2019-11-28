45fe4530294fe5152d525c5e768b64641ec864b6
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2019-11-28 09:09:22 +0100

    Gjort ting pænere

--- AMS/ViewModels/ShortcutsListViewModel.cs
+++ AMS/ViewModels/ShortcutsListViewModel.cs
@@ -8,7 +8,7 @@ namespace AMS.ViewModels
     {
         public Dictionary<string, Dictionary<string, string>> ShortcutsList { get; set; }
 
-        // TODO: Fix loophole med at kunne tilgå ting man ikke må gennem shortcuts
+        // TODO: Gør mig pæn
 
         public ShortcutsListViewModel()
         {
@@ -21,21 +21,21 @@ namespace AMS.ViewModels
             ShortcutsList["General"].Add("Alt + 4", "Users");
             ShortcutsList["General"].Add("Alt + 5", "Log");
 
-            ShortcutsList.Add("List view", new Dictionary<string, string>());
-            ShortcutsList["List view"].Add("Delete / Ctrl + D ", "Delete item");
-            ShortcutsList["List view"].Add("Enter / Ctrl + W", "View item");
-            ShortcutsList["List view"].Add("Ctrl + E", "Edit item");
-            ShortcutsList["List view"].Add("Crtl + P", "Export item(s)");
-            ShortcutsList["List view"].Add("Ctrl + N", "Add new item");
+            ShortcutsList.Add("Overview", new Dictionary<string, string>());
+            ShortcutsList["Overview"].Add("Delete / Ctrl + D ", "Delete item");
+            ShortcutsList["Overview"].Add("Enter / Ctrl + W", "View item");
+            ShortcutsList["Overview"].Add("Ctrl + E", "Edit item");
+            ShortcutsList["Overview"].Add("Crtl + P", "Export item(s)");
+            ShortcutsList["Overview"].Add("Ctrl + N", "Add new item");
             //ShortcutsList["List view"].Add("Ctrl + F", "Search items");
 
             ShortcutsList.Add("Edit item", new Dictionary<string, string>());
-            ShortcutsList["Edit item"].Add("Ctrl + S", "Save item / changes");
+            ShortcutsList["Edit item"].Add("Ctrl + S", "Save item / Changes");
             ShortcutsList["Edit item"].Add("Ctrl + Shift + S", "Save copy of current item");
             ShortcutsList["Edit item"].Add("Esc / Ctrl + Q", "Cancel / Go back");
 
-            ShortcutsList.Add("Asset view", new Dictionary<string, string>());
-            ShortcutsList["Asset view"].Add("Ctrl + Enter", "Save new comment");
+            ShortcutsList.Add("Asset inspection", new Dictionary<string, string>());
+            ShortcutsList["Asset inspection"].Add("Ctrl + Enter", "Save new comment");
 
             OnPropertyChanged(nameof(ShortcutsList));
         }
