d271ce61757f6655a770feb9acfb1f2aada1582e
Alexander Nykjær <alax0013@gmail.com>
2019-12-06 09:07:19 +0100

    Button is now disabled when name isnt set.

--- AMS/ViewModels/Prompts/CustomFieldViewModel.cs
+++ AMS/ViewModels/Prompts/CustomFieldViewModel.cs
@@ -16,6 +16,7 @@ namespace AMS.ViewModels.Prompts
 
         public override event PromptEventHandler PromptElapsed;
 
+        public bool IsFieldNameNotNull => !string.IsNullOrEmpty(Name);
         public string Name { get; set; } = String.Empty;
         public string DefaultValue { get; set; } = String.Empty;
         public bool DefaultBool
