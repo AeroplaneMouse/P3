9fa68c5914480aa5c4e4afbe24d229c46951a674
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2019-12-15 12:14:56 +0100

    Lidt mere redigering af kommentarer, mangler bare cancel

--- AMS/Views/ValueConverters/CommentNotEditingToVisibilityConverter.cs
+++ AMS/Views/ValueConverters/CommentNotEditingToVisibilityConverter.cs
@@ -7,11 +7,11 @@ using System.Windows.Data;
 
 namespace AMS.Views.ValueConverters
 {
-    public class CommentEditingToVisibilityConverter : IValueConverter
+    public class CommentNotEditingToVisibilityConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
-            return (bool)value ? Visibility.Visible : Visibility.Hidden;
+            return (bool)value ? Visibility.Hidden : Visibility.Visible;
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
