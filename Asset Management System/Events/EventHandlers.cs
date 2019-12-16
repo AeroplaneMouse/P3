88a9e5b6a6588b695c04ae8a33ab464135d97728
Ane Søgaard Jørgensen <anesoejoe@gmail.com>
2019-12-16 12:37:57 +0100

    Fjernet gammelt system

--- Asset Management System/Events/EventHandlers.cs
+++
@@ -1,8 +0,0 @@
-﻿using Asset_Management_System.Models;
-
-namespace Asset_Management_System.Events
-{
-    public delegate void NotificationEventHandler(Notification n);
-
-    public delegate void SqlConnectionEventHandler();
-}
